4fa5159d462dc856bb7a9ddbaa824332548af349
Irishtheboy <109201974+Irishtheboy@users.noreply.github.com>
2024-10-07 10:55:52 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/adp2group32/ADP2Group32.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adp2group32;
+
+/**
+ *
+ * @author Franco
+ */
+public class ADP2Group32 {
+
+    public static void main(String[] args) {
+        
+    }
+}
